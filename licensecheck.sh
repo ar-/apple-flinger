81aa19acbb8de0388dc302b8bad39f41231d127b
Andreas Redmer <andreasredmer@mailchuck.com>
2017-09-22 05:29:33 +0800

    removed android sdk from licence check

--- licensecheck.sh
+++ licensecheck.sh
@@ -1,9 +1,9 @@
-RET=`licensecheck \`find . -name "*.java"\` | grep -v "GPL (v3 or later)" | grep -v "GENERATED FILE" | wc -l`
+RET=`licensecheck -i "android-sdk-linux" \`find . -name "*.java"\` | grep -v "GPL (v3 or later)" | grep -v "GENERATED FILE" | wc -l`
 if [ $RET -eq 0 ]; then
 	echo all copyrights okay
 	exit $RET
 else
-	echo there are $RET files with wrong copyright
-	licensecheck `find . -name "*.java"` | grep -v "GPL (v3 or later)" | grep -v "GENERATED FILE"
+	licensecheck -i "android-sdk-linux" `find . -name "*.java"` | grep -v "GPL (v3 or later)" | grep -v "GENERATED FILE"
+	echo ERROR : there are $RET files with wrong copyright - listed above
 	exit 1
 fi
