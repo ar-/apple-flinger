8361cb94c88d2ffed3ee217aac92559300f32390
Andreas Redmer <andreasredmer@mailchuck.com>
2017-09-22 05:45:36 +0800

    added checksum checks for full-text license files

--- licensecheck.sh
+++ licensecheck.sh
@@ -1,9 +1,26 @@
+# check source
 RET=`licensecheck -i "android-sdk-linux" \`find . -name "*.java"\` | grep -v "GPL (v3 or later)" | grep -v "GENERATED FILE" | wc -l`
 if [ $RET -eq 0 ]; then
 	echo all copyrights okay
-	exit $RET
 else
 	licensecheck -i "android-sdk-linux" `find . -name "*.java"` | grep -v "GPL (v3 or later)" | grep -v "GENERATED FILE"
 	echo ERROR : there are $RET files with wrong copyright - listed above
 	exit 1
 fi
+
+#check COPYING files
+LICSHA=`find . -name "COPYING" | grep -v android-sdk-linux | xargs sha512sum | egrep -o "[a-f0-9]+ " | sort -u`
+LICMD5=`find . -name "COPYING" | grep -v android-sdk-linux | xargs    md5sum | egrep -o "[a-f0-9]+ " | sort -u`
+if [ $LICSHA = "7633623b66b5e686bb94dd96a7cdb5a7e5ee00e87004fab416a5610d59c62badaf512a2e26e34e2455b7ed6b76690d2cd47464836d7d85d78b51d50f7e933d5c" ]; then
+	if [ $LICMD5 = "d32239bcb673463ab874e80d47fae504" ]; then
+		echo all license files okay
+	else
+		echo ERROR : MD5 of COPYING file has changed
+		exit 1
+	fi
+else
+	echo ERROR : SHA of COPYING file has changed
+	exit 1
+fi
+
+
