245b66025530f766687c5495769ba6856b0ef976
Andreas Redmer <andreasredmer@mailchuck.com>
2017-09-29 10:28:00 +0000

    Revert "Merge branch 'google-play' into 'master'"
    This reverts commit f034fddffec56fc45f6a246bbf77e27e605dbbe5

--- android/src/com/gitlab/ardash/appleflinger/android/AndroidLauncher.java
+++ android/src/com/gitlab/ardash/appleflinger/android/AndroidLauncher.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  ******************************************************************************/
-package com.ardash.appleflinger.android;
+package com.gitlab.ardash.appleflinger.android;
 
 import java.util.List;
 
