93ec852913354fae3c2108baea93b3b341531358
Andreas Redmer <andreasredmer@mailchuck.com>
2017-10-14 10:08:35 +0800

    finalized screenshot factory, ran it over all languages

--- tests/src-integration/com/gitlab/ardash/appleflinger/testing/integration/DesktopTestdriveTest.java
+++ tests/src-integration/com/gitlab/ardash/appleflinger/testing/integration/DesktopTestdriveTest.java
@@ -16,11 +16,13 @@
  ******************************************************************************/
 package com.gitlab.ardash.appleflinger.testing.integration;
 
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.lwjgl.openal.AL;
+import org.lwjgl.util.vector.Matrix;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
@@ -31,6 +33,7 @@ import com.gitlab.ardash.appleflinger.AppleflingerGame;
 import com.gitlab.ardash.appleflinger.ai.PlayerSimulator;
 import com.gitlab.ardash.appleflinger.global.Assets;
 import com.gitlab.ardash.appleflinger.global.GameManager;
+import com.gitlab.ardash.appleflinger.global.GameState;
 import com.gitlab.ardash.appleflinger.helpers.Pref;
 import com.gitlab.ardash.appleflinger.i18n.I18N;
 import com.gitlab.ardash.appleflinger.missions.Mission;
@@ -38,6 +41,11 @@ import com.gitlab.ardash.appleflinger.screens.MissionSelectScreen;
 
 
 public class DesktopTestdriveTest implements ActionResolver{
+	
+	private static final String [] langs = {"de","en","es","fr"};
+	//private static final String [] langs = {"ru"}; // separate non-latin load
+	private static final Mission [] missions = {Mission.M_1_1, Mission.M_1_3, Mission.M_1_4
+		,Mission.M_1_5, Mission.M_1_11, Mission.M_1_14};
 
 
 	@BeforeClass
@@ -48,39 +56,61 @@ public class DesktopTestdriveTest implements ActionResolver{
 	}
 	
 	@Test
-	public void takeScreenShotsEN() {
-		Pref.setLingo("en");
-		I18N.loadLanguageBundle(Pref.getLingo());
-//		sleep(3000);
-		final Mission missionToLoad = Mission.M_1_1;
-		
-		loadMission(missionToLoad);
-		sleep(1000);
-		PlayerSimulator.INSTANCE.playOneRound();
-		sleep(5000);
-		PlayerSimulator.INSTANCE.playOneRound();
-		sleep(2500);
-		saveScreenShot("s01en.png");
-		sleep(10000);
+	public void takeScreenShots() {
+		GameManager gm = GameManager.getInstance();
+		for (final Mission missionToLoad : missions) {
+			for (String lang : langs) {
+				Pref.setLingo(lang);
+				I18N.loadLanguageBundle(Pref.getLingo());
+				// assign player names typical for the language
+				Pref.setPlayer1Name(getPlayerName(lang, 1));
+				Pref.setPlayer2Name(getPlayerName(lang, 2));
+				
+				loadMission(missionToLoad);
+				
+				sleep(5000); // some setups shake for 5 seconds
+				PlayerSimulator.INSTANCE.playOneRound();
+				sleep(2500); // 3 pull + 1 expire
+				for (int i = 0 ; i< 60; i++)
+				{
+					sleep(1000); // wait for swingout
+					if (gm.getGameState()==GameState.WAIT_FOR_DRAG)
+						break;
+				}
+				PlayerSimulator.INSTANCE.playOneRound();
+				sleep(4200);
+				saveScreenShot("../metadata/"+lang+"/images/phoneScreenshots/"+missionToLoad+".png");
+				sleep(1000);
+				
+			}
+		}
 	}
 
-	@Test
-	public void takeScreenShotsFR() {
-		Pref.setLingo("fr");
-		I18N.loadLanguageBundle(Pref.getLingo());
-		final Mission missionToLoad = Mission.M_1_1;
+	private static String getPlayerName(String lang, int player) 
+	{
+		String ret = null;
+		HashMap<String, String> n1 = new HashMap<String, String>();
+		HashMap<String, String> n2 = new HashMap<String, String>();
 		
-		loadMission(missionToLoad);
-//		saveScreenShot("s01fr.png");
-		sleep(1000);
-		PlayerSimulator.INSTANCE.playOneRound();
-		sleep(5000);
-		PlayerSimulator.INSTANCE.playOneRound();
-		sleep(2900);
-		saveScreenShot("s01fr.png");
-		sleep(10000);
-		AL.destroy();
-		Gdx.app.exit();
+		n1.put("de", "Stefan".toUpperCase());
+		n2.put("de", "Bianka".toUpperCase());
+		n1.put("en", "Ryan".toUpperCase());
+		n2.put("en", "Amber".toUpperCase());
+		n1.put("es", "Nicolás".toUpperCase());
+		n2.put("es", "Sofía".toUpperCase());
+		n1.put("fr", "Pierre".toUpperCase());
+		n2.put("fr", "Debonnaire".toUpperCase());
+		n1.put("ru", "Михаи́л".toUpperCase());
+		n2.put("ru", "Светлана".toUpperCase());
+		
+		if (player == 1)
+			ret = n1.get(lang);
+		if (player == 2)
+			ret = n2.get(lang);
+		
+		if (ret==null)
+			throw new RuntimeException("no name found for "+lang+player);
+		return ret;
 	}
 
 	private static void loadMission(final Mission missionToLoad) {
@@ -140,62 +170,56 @@ public class DesktopTestdriveTest implements ActionResolver{
 
 	@Override
 	public void restartMySelf() {
-		// TODO Auto-generated method stub
-		
+		// not used in this test implementation
 	}
 
 	@Override
 	public boolean twPostRecommendation() {
-		// TODO Auto-generated method stub
+		// not used in this test implementation
 		return false;
 	}
 
 	@Override
 	public boolean fbPostRecommendation() {
-		// TODO Auto-generated method stub
+		// not used in this test implementation
 		return false;
 	}
 
 	@Override
 	public boolean gpPostRecommendation() {
-		// TODO Auto-generated method stub
+		// not used in this test implementation
 		return false;
 	}
 
 	@Override
 	public boolean piPostRecommendation() {
-		// TODO Auto-generated method stub
+		// not used in this test implementation
 		return false;
 	}
 
 	@Override
 	public void submitScoreGPGS(String LeaderBoardId, int score) {
-		// TODO Auto-generated method stub
-		
+		// not used in this test implementation
 	}
 
 	@Override
 	public void unlockAchievementGPGS(String achievementId) {
-		// TODO Auto-generated method stub
-		
+		// not used in this test implementation
 	}
 
 	@Override
 	public void incrementAchievementGPGS(String achievementId, int NumSteps) {
-		// TODO Auto-generated method stub
-		
+		// not used in this test implementation
 	}
 
 	@Override
 	public void getLeaderboardGPGS(String LeaderBoardId) {
-		// TODO Auto-generated method stub
-		
+		// not used in this test implementation
 	}
 
 	@Override
 	public void getAchievementsGPGS() {
-		// TODO Auto-generated method stub
-		
+		// not used in this test implementation
 	}
 
 }
