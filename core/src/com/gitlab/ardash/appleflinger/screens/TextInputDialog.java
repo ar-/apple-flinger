75a8a1c67c9653a012a8b8985b57972dee0dfb04
Andreas Redmer <ar-appleflinger@abga.be>
2018-10-24 00:43:09 +0800

    adde Galacian and Dutch keyboard

--- core/src/com/gitlab/ardash/appleflinger/screens/TextInputDialog.java
+++ core/src/com/gitlab/ardash/appleflinger/screens/TextInputDialog.java
@@ -151,7 +151,7 @@ public class TextInputDialog extends AdvancedDialog{
 			tblContent.row();
 			tblContent.add(makeKeyboardRow("ZĈCVBNMĤ"));
 			tblContent.row();
-		} else if (langCode.equals("es") || langCode.equals("pt")) {
+		} else if (langCode.equals("es") || langCode.equals("gl") || langCode.equals("pt")) {
 			tblContent.row();
 			tblContent.add(makeKeyboardRow("QWERTYUIOPÜÚ"));
 	        tblContent.add(btnBackSpace);
