ca4b20c130a755d3846f6213f5ce2ff2e05c1c40
Andreas Redmer <ar-appleflinger@abga.be>
2018-10-23 20:23:49 +0800

    fixed author list and script

--- AUTHORS.md
+++ AUTHORS.md
@@ -4,14 +4,12 @@ We'd like to thank the following people for their contributions:
 
 
 - Allan Nordhøy \<epost⒜anotheragency∙no\>
-- Andreas \<a_r⒜posteo∙de\>
 - Andreas Redmer \<ar-appleflinger⒜abga∙be\>
 - ButterflyOfFire \<ButterflyOfFire⒜protonmail∙com\>
 - Dmitry Mikhirev \<mikhirev⒜gmail∙com\>
 - Heimen Stoffels \<vistausss⒜outlook∙com\>
 - Iván \<ivanrsm1997⒜gmail∙com\>
 - Markel \<wakutiteo⒜protonmail∙com\>
-- vagrant \<vagrant⒜vagrant∙vm\>
 - Verdulo \<cybertomek⒜openmailbox∙org\>
 - WaldiS \<admin⒜sto∙ugu∙pl\>
 - xin \<xin⒜riseup∙net\>
--- scripts/update-authors-file.sh
+++ scripts/update-authors-file.sh
@@ -27,7 +27,7 @@ echo -e "Authors\n=======\nWe'd like to thank the following people for their con
 git log --raw | grep "^Author: " | sort | uniq | cut -d ' ' -f2- | sed 's/^/- /' >> /tmp/AUTHORS.md
 
 # remove authors that don't want to be inlcuded or old email addresses 
-for AE in ar-gitlab@abga.be andreasredmer@mailchuck.com michal@cihar.com hosted@weblate.org
+for AE in ar-gitlab@abga.be andreasredmer@mailchuck.com a_r@posteo.de vagrant@vagrant.vm michal@cihar.com hosted@weblate.org
 do
   echo excluding $AE
   cat /tmp/AUTHORS.md | grep -v "$AE" > /tmp/AUTHORS_tmp.md
