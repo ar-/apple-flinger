5d0e24780f4779031d23fa0de463e00e82427c6a
Dmytro Vasyliovych Klymenko <DmytroVasyliovychKlymenko@super-sm.art>
2023-04-14 11:35:13 +0200

    updated licenses

--- core/src/com/gitlab/ardash/appleflinger/actors/Jet.java
+++ core/src/com/gitlab/ardash/appleflinger/actors/Jet.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (C) 2015-2018 Andreas Redmer <ar-appleflinger@abga.be>
+ * Copyright (C) 2023 Dmytro Vasyliovych Klymenko <DmytroVasyliovychKlymenko@super-sm.art>
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
